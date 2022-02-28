854acb8d5a3a935fe6a061d00815a8ba560c380b
banlonjones <banlonjones@go-groups.net>
2022-02-28 11:11:09 +0100

    empty navbar added and duck pattern implemented

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import logo from './planet.png';
+
+function Navbar() {
+  return (
+    <div>
+      <nav className="container-fluid navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+          <span className="navbar-brand">
+            <img src={logo} height="60" alt="brand logo" />
+            Space Travellers Hub
+          </span>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+          <div className="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+            <ul className="navbar-nav ml-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a href="/#" className="nav-link active">Rockets</a>
+              </li>
+              <li className="nav-item">
+                <a href="/#" className="nav-link">Missions</a>
+              </li>
+              <li className="nav-item">
+                <a href="/#" className="nav-link">My porfile</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default Navbar;
