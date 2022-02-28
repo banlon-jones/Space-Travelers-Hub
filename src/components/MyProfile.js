62ff1221aeb2388dee229e99e0475d8a1f0ab67f
banlonjones <banlonjones@go-groups.net>
2022-02-28 23:13:45 +0100

    my missions displayed on myprofile

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -1,8 +1,28 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 function MyProfile() {
+  const missions = useSelector(({ missionReducer }) => missionReducer);
+  const myMissions = missions.filter((item) => item.status);
   return (
-    <div className="my-profile" />
+    <div className="my-profile">
+      <div className="container mt-4">
+        <div className="row">
+          <div className="col-6">
+            <div className="card">
+              <div className="card-header">
+                <h3 className="card-title">My Missions</h3>
+              </div>
+              <ul className="list-group list-group-flush">
+                {
+                  myMissions.map((item) => <li key={item.id} className="list-group-item">{item.name}</li>)
+                }
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
