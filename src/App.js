6c5de79f477e7cca367efef79ef0fe986bbaf02a
Vana Seraydarian <vanaseraydarian123@gmail.com>
2022-02-28 17:52:51 +0200

    Update files for reserve, cancel button

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/navbar/Navbar';
+import Line from './components/Line';
 import Rockets from './components/Rockets';
 import MyProfile from './components/MyProfile';
 import MissionPage from './pages/mission-page/MissionPage';
@@ -12,6 +13,7 @@ function App() {
       <Router>
         <header>
           <Navbar />
+          <Line />
         </header>
         <Routes>
           <Route path="/" element={<MyProfile />} />
