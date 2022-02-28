736444b34a7697f5d75e7aa1c7b16b15e3809c2c
banlonjones <banlonjones@go-groups.net>
2022-02-28 22:04:20 +0100

    bug fixed

--- src/pages/mission-page/MissionPage.js
+++ src/pages/mission-page/MissionPage.js
@@ -5,9 +5,6 @@ function MissionPage() {
   return (
     <div>
       <div className="container-fluid">
-        <h1> Welcome to missions </h1>
-      </div>
-      <div className="container-fluid">
         <ListMission />
       </div>
     </div>
