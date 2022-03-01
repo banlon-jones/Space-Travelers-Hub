fbd94e0f81262236ea525dd2f3d7f1a529795e31
banlonjones <banlonjones@go-groups.net>
2022-03-01 08:56:45 +0100

    merged develop

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './redux/configureStore';
 import './index.css';
-import { Provider } from 'react-redux';
 import App from './App';
 
 ReactDOM.render(
